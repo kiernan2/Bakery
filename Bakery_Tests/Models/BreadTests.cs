1ac7b43a227ebc0bf3a0418aabf33e745a677220
Kiernan2 <kiernan1994@gmail.com>
2022-04-15 15:42:40 -0700

    More tests

--- Bakery_Tests/Models/BreadTests.cs
+++ Bakery_Tests/Models/BreadTests.cs
@@ -9,6 +9,18 @@ namespace BreadTests
   public class TestClass
   {
     [TestMethod]
+    public void one_bread_price_check()
+    {
+      Bread newBread = new Bread(1);
+      Assert.AreEqual(newBread.Price, 5);
+    }
+    [TestMethod]
+    public void two_bread_price_check()
+    {
+      Bread newBread = new Bread(2);
+      Assert.AreEqual(newBread.Price, 10);
+    }
+    [TestMethod]
     public void three_bread_price_check()
     {
       Bread newBread = new Bread(3);
--- Bakery_Tests/Models/PastryTests.cs
+++ Bakery_Tests/Models/PastryTests.cs
@@ -9,10 +9,40 @@ namespace PastryTests
   public class TestClass
   {
     [TestMethod]
+    public void one_pastry_price_check()
+    {
+      Pastry newPastry = new Pastry(1);
+      Assert.AreEqual(newPastry.Price, 2);
+    }
+    [TestMethod]
+    public void two_pastry_price_check()
+    {
+      Pastry newPastry = new Pastry(2);
+      Assert.AreEqual(newPastry.Price, 4);
+    }
+    [TestMethod]
     public void three_pastry_price_check()
     {
       Pastry newPastry = new Pastry(3);
       Assert.AreEqual(newPastry.Price, 5);
+    }
+    [TestMethod]
+    public void four_pastry_price_check()
+    {
+      Pastry newPastry = new Pastry(4);
+      Assert.AreEqual(newPastry.Price, 7);
+    }
+        [TestMethod]
+    public void five_pastry_price_check()
+    {
+      Pastry newPastry = new Pastry(5);
+      Assert.AreEqual(newPastry.Price, 9);
+    }
+    [TestMethod]
+    public void six_pastry_price_check()
+    {
+      Pastry newPastry = new Pastry(6);
+      Assert.AreEqual(newPastry.Price, 10);
     }
   }
 }
