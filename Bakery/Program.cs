a3b3cdcb4f1e8c5c0c32930404bc61096ceb95d6
Kiernan2 <kiernan1994@gmail.com>
2022-04-15 15:42:58 -0700

    Front end

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -6,12 +6,15 @@ public class Program
 {
   static void Main()
   {
-    int breadPrice = 0;
-    int pastryPrice = 0;
     Console.WriteLine("Welcome to Pierre's bakery");
-    Console.WriteLine("Enter how much bread you want");
+    Console.WriteLine("Enter how much bread you want price is five dollars a loaf with a buy two get one free deal");
     string breadResponse = Console.ReadLine();
-    Console.WriteLine("Enter how many pastries you want");
+    Bread breadPrice = new Bread(Int32.Parse(breadResponse));
+    Console.WriteLine("Enter how many pastries you want Prices two dollars a pastry with a one dollar discount for every third");
     string pastriesResponse = Console.ReadLine();
+    new Pastry(Int32.Parse(pastriesResponse));
+    Pastry pastryPrice = new Pastry(Int32.Parse(pastriesResponse));
+    int total = breadPrice.Price + pastryPrice.Price;
+    Console.WriteLine($"Your total cost is {total}$");
   }
 }
